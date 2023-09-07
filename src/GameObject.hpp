841546ab1977e1c9b04a1827634ca319d52b9db3
LemonTea387 <JunJieLow08@gmail.com>
2023-09-07 23:16:24 +0800

    GameObject generalization

---
+++ src/GameObject.hpp
@@ -0,0 +1,19 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include <SDL_render.h>
+
+class GameObject {
+public:
+  GameObject();
+  ~GameObject();
+  virtual void render(SDL_Renderer *renderer) const;
+  SDL_Rect getBounds() const { return m_Bounds; };
+
+private:
+  SDL_Rect m_Bounds;
+  SDL_Texture *m_Texture{nullptr};
+  SDL_Rect m_TextureRect;
+};
+
+#endif
