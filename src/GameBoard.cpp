bed8c711388ffae31afa3eda2cc1ec7521c1a671
LemonTea387 <JunJieLow08@gmail.com>
2023-09-11 21:24:10 +0800

    Another math error :(

--- src/GameBoard.cpp
+++ src/GameBoard.cpp
@@ -13,7 +13,7 @@ void GameBoard::render(SDL_Renderer &renderer) const {
   SDL_Rect tileRect{0, 0, TILE_SIZE_X, TILE_SIZE_Y};
   SDL_SetRenderDrawColor(&renderer, 160, 160, 160, 255);
   for (int i = 0; i < m_Board.size(); i++) {
-    tileRect.x = (i % m_Dimension.second) * TILE_SIZE_X;
+    tileRect.x = (i % m_Dimension.first) * TILE_SIZE_X;
     tileRect.y = (i / m_Dimension.first) * TILE_SIZE_Y;
     SDL_RenderDrawRect(&renderer, &tileRect);
   }
